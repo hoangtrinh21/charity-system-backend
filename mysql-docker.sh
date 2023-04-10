65a401bebc58fe45363c9277618f3098bc0d6e20
hoangt <hoangtrinh@admicro.vn>
2023-04-10 15:30:45 +0700

    docker

--- mysql-docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm --name mysql-charity-hoangtrinh -v mysql-charity-data:/var/lib/mysql -v mysql-charity-config-deamond:/etc/mysql/conf.d -p 3310:3306 -p 33100:33060 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_USER=dev -e MYSQL_PASSWORD=123456 -e MYSQL_DATABASE=charity --network charity-network mysql:latest
