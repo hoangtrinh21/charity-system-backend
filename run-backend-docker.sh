8a0a58dc9d1fcbb6c50024b36fdfc851586f392e
hoangt <hoangtrinh@admicro.vn>
2023-04-11 11:15:44 +0700

    docker

--- run-backend-docker.sh
+++ run-backend-docker.sh
@@ -1,4 +1,4 @@
 # linux
-docker run -d -p 8089:8089 --name backend-hoangtrinh -v "$(pwd):/app" --network charity-network trinhhoang19020301/spring-boot-docker:1.0.0.1
+docker run -d -p 8089:8089 --name backend-hoangtrinh -v "$(pwd):/app" --network charity-network trinhhoang19020301/charity-backend:1.0.0.0
 # windows
 #docker run --rm -p 8089:8089 --name backend-hoangtrinh -v "%cd%:/app" --network charity-network trinhhoang19020301/spring-boot-docker:1.0.0.1
