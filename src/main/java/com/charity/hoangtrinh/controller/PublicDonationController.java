9856e4b2dd4ca9ee6c72c447a4f0c9a37b2bf0df
hoangtrinh21 <trinhhoang2001bn@gmail.com>
2023-04-13 01:46:35 +0700

    database

--- src/main/java/com/charity/hoangtrinh/controller/PublicDonationController.java
+++ src/main/java/com/charity/hoangtrinh/controller/PublicDonationController.java
@@ -1,10 +1,14 @@
 package com.charity.hoangtrinh.controller;
 
+import com.charity.hoangtrinh.dbs.sql.charitydatabase.repositories.PublicDonationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/charity/public-donation")
 @RestController
 public class PublicDonationController {
+    @Autowired
+    private PublicDonationRepository publicDonationRepository;
 
 }
