4d1c9301c236bcb3dd942e28043456c0e323508b
hoangtrinh21 <trinhhoang2001bn@gmail.com>
2023-04-16 18:16:22 +0700

    database

--- src/main/java/com/charity/hoangtrinh/controller/AccessController.java
+++ src/main/java/com/charity/hoangtrinh/controller/AccessController.java
@@ -9,9 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/charity/access")
 public class AccessController {
     @PostMapping("/token")
@@ -20,12 +22,14 @@ public class AccessController {
             JSONObject jsonBody = new JSONObject(body);
             int userId = jsonBody.getInt("user_id");
             String token = jsonBody.getString("token");
+            System.out.println(userId + ": " + token);
             CacheConfig.accessToken.put(userId, token);
 
+            System.out.println(CacheConfig.accessToken.asMap());
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new ResponseModel(HttpStatus.OK.value(),
                             "Cached access token of user " + userId,
-                            "{}}"));
+                            "{}"));
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
