b4e711d8ec7735b0994aa81bca3458de91be9f74
hoangt <hoangtrinh@admicro.vn>
2023-05-05 10:28:10 +0700

    new

--- charity.sql
+++ charity.sql
@@ -19,6 +19,12 @@
 -- Table structure for table `administrative_regions`
 --
 
+DROP DATABASE IF EXIST `charity`;
+
+CREATE DATABSE `charity`;
+
+USE `charity`;
+
 DROP TABLE IF EXISTS `administrative_regions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
